57948f9db58f0c40363e3dfbb576ad829549b2b6
Robert Ziegler <zieglerrp@protonmail.com>
2019-11-10 08:54:21 -0800

    Session_4

--- FSDI_108/session_2/Project/ERP/src/app/user-list/user-list.component.ts
+++ FSDI_108/session_2/Project/ERP/src/app/user-list/user-list.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { User } from '../models/User';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent {
+  userList: User[] = [];
 
   constructor(private data: DataService) {
     // read data from service
-    console.log("On data Service", data.getAllUsers());
-  }
-
-  ngOnInit() {
+    this.userList = data.getAllUsers();
+    console.log("User list has: ", this.userList.length);
   }
 
 
--- FSDI_108/session_2/Project/ERP/src/app/user-register/user-register.component.ts
+++ FSDI_108/session_2/Project/ERP/src/app/user-register/user-register.component.ts
@@ -11,19 +11,65 @@ export class UserRegisterComponent implements OnInit {
 
   model: User = new User();
   retypePassword: string;
+  userNameError = false;
+  passwordError = false;
+  passMatchError = false;
+  emailError = false;
 
   constructor(private data: DataService) {}
 
   ngOnInit() {
   }
 
-  registerUser() {
+  userValueChanged() {
+    this.userNameError = this.model.userName.length < 4;
+  }
 
+  passValueChanged() {
+    this.passwordError = this.model.password.length < 6;
+
+    this.passMatchError = this.model.password != this.retypePassword;
+  }
+
+  emailValueChanged() {
+    if(this.model.email.length < 8 || !this.model.email.includes('@') ) {
+      this.emailError = true;
+    } else {
+       this.emailError = false;
+    }
+
+  }
+
+  registerUser() {
+  
+    var isThereAnError = false;
+    if(!this.model.userName) {
+      this.userNameError = true;
+      isThereAnError = true;
+    
+    }
+    if(!this.model.password) {
+      this.passwordError = true;
+      isThereAnError = true;
+    }
+    if(!this.model.email) {
+      this.emailError = true;
+      isThereAnError = true;
+    }
+
+    if(isThereAnError || this.passMatchError || this.emailError) {
+      return;
+    }
     console.log("The user twant to another user");
     console.log(this.model);
     
     // save the user into the service
     this.data.saveUser(this.model);
+
+    // create a new one
+    // so user can register more users
+    this.model = new User();
+    this.retypePassword = "";
   }
 
 
