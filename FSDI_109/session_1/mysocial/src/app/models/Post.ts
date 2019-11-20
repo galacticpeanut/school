08bce993903ee47801e1fd7315d704fa60ce7167
Robert Ziegler <zieglerrp@protonmail.com>
2019-11-19 16:02:05 -0800

    Session_1

---
+++ FSDI_109/session_1/mysocial/src/app/models/Post.ts
@@ -0,0 +1,14 @@
+export class Post {
+  // class attributes
+  public message: string;
+  public imageUrl: string;
+  public from: string;
+  public to: string;
+  public createdOn: Date;
+
+  //class methods
+  constructor() {
+    this.to = "Everyone";
+    this.createdOn = new Date(); // new Date() == get the exact time on the moment
+  }
+}
---
+++ FSDI_109/session_1/mysocial/src/app/tab2/tab2.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Post } from '../models/Post';
+import { DataService } from '../services/data.service';
+import { SharedService } from '../services/shared.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  model = new Post();
+  constructor(private data: DataService, private shared: SharedService) {
+
+  }
+
+  /**
+   * create a Shared service
+   * put a userName attribute in the service
+   * Inject the service into this component
+   * change ln 28 to this.model.from = this.shared.userName;
+   */
+
+  sendPost() {
+
+    if (!this.model.message && !this.model.imageUrl) return;
+
+    this.model.createdOn = new Date(); // set this moment on time
+    this.model.from = this.shared.userName;
+    console.log("Saving post", this.model);
+
+    // save the obj
+    this.data.savePost(this.model);
+
+    // create new model (clear form)
+    this.model = new Post();
+  }
+}
